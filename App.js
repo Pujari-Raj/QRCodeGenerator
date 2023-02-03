18674775992fb28be025fc1aeb0e4f8e8ba7331d
Pujari-Raj <basvarajpujari607@gmail.com>
2023-02-04 02:28:40 +0530

    changes updated and added screenshot

--- App.js
+++ App.js
@@ -1,33 +1,17 @@
 //Gathering elements
-const generatebtn = document.querySelector(".submit");
-let qr_code_img = document.querySelector(".qrimg");
-let userinput = document.querySelector(".data");
+const generatebtn = document.getElementById("Submit");
+let qr_code_img = document.getElementById("img");
+let userinput = document.getElementById("input");
 
-// EventListener on button-click
-generatebtn.addEventListener('click', () => {
+const apiUrl ="https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=";
+
+generatebtn.addEventListener("click",generateqrcode);
+
+function generateqrcode  () {
     console.log('button clicked');
-    if(userinput.value !== ""){
-        generate(userinput);
-    }
-    else{
-        alert("Provide a valid input");
-    }
-})
-
-// function to generate QR-Code after taking user's input
-function generate(user_input){
-    console.log("generate called!!!");
-    qr_code_img.style = "";
-
-    //Using the Object of QRCODE from qrcode library
-    var qr_code = new QRCode(qr_code_img,{
-        text : `${user_input.value}`,
-        width: 150, //128
-        height: 150,
-        colorDark: "#000000",
-        colorLight: "#ffffff",
-        correctLevel : QRCode.CorrectLevel.H
-    });
+    let url = apiUrl+encodeURIComponent(userinput.value);
+    fetch(url).then((response) => response).then((data) => {
+        qr_code_img.style.display = "block";
+        qr_code_img.src = data.url;
+      });
 }
-
-generate(userinput);
---
+++ Readme.md
@@ -0,0 +1,14 @@
+## This is Accordion App which was given as task in FSJSBootcamp.
+
+
+Technologies/ Tools Used:
+
+- HTML
+- CSS
+- Javascript
+
+
+### Project Preview
+
+![Project-Image](/project_ss.png)
+
